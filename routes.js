fd87d6e4cc8339922085fc5fdaeb4c816d7777c2
niawkung <niawkung2@gmail.com>
2017-10-28 08:34:20 +0700

    fix

--- routes.js
+++ routes.js
@@ -4,5 +4,9 @@ const routes = nextRoutes();
 
 routes.add('home', '/', 'index');
 routes.add('test', '/test/:testId', 'test');
-
+routes.add('register', '/register');
+routes.add('login', '/login');
+routes.add('interested_language', '/interested_language');
+routes.add('interested_people', '/interested_people');
+routes.add('profile', '/profile');
 module.exports = routes;
